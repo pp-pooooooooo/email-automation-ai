0ffec5656bd6b7caaa80c09484cced6e194be274
pp-pooooooooo <ppdpyp@gmail.com>
2026-03-18 17:22:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,16 @@ from sheets_client import get_sheet
 from summarizer import summarize_email
 
 
+import os
+from google.oauth2.credentials import Credentials
+
 def get_credentials():
     creds = None
 
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-    if not creds or not creds.valid:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES
-        )
-        creds = flow.run_local_server(port=0)
-
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
     return creds
-
 
 def run():
     creds = get_credentials()

9ce137f1b8d6208cc342656df1912132107e14e9
pp-pooooooooo <ppdpyp@gmail.com>
2026-03-18 16:43:36 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/spreadsheets'
+]
+
+OPENAI_API_KEY = "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"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1SllXNThBo9Uap2577pQLgw5E91tH-Bwq3WzrjS87DHo/edit?usp=sharing"
